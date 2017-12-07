7d58f3344155d9281147228cd6ac8a97b2dfdcdf
Mauli Pandey <pandeymauli@Maulis-MacBook-Pro.local>
2017-12-07 12:52:07 -0500

    v5-more-fixes

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret.
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+
+# Note: A secret key is included in the sample so that it works, but if you
+# use this code in your application please replace this with a truly secret
+# key. See http://flask.pocoo.org/docs/0.12/quickstart/#sessions.
+SECRET_KEY = 'REPLACE ME - this value is here as a placeholder.'
+SQLALCHEMY_DATABASE_URI = "postgresql://localhost/chatdb2"
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
